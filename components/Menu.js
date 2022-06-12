e298a6fbf70235877e602f5b0348b8dd21f6922f
Ksarfo69 <ksarfo69@gmail.com>
2022-06-12 20:40:56 +0000

    added links to menu on mobile

--- components/Menu.js
+++ components/Menu.js
@@ -1,10 +1,29 @@
 import styles from '../styles/Menu.module.css'
 import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import MenuLinks from './MenuLinks';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Menu = () => {
+
+    const [openMenu, setopenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setopenMenu(!openMenu)
+    }
+
+    console.log(openMenu)
+
   return (
+      <div>
+      
     <div className={styles.container}>
-        <div className={styles.iconholder}><MenuIcon style={{width: "50px", height: "50px", color: "white"}}/></div>
+        <div className={styles.iconholder}>
+            { openMenu? <CloseIcon onClick={()=>toggleMenu()} style={{width: "50px", height: "50px", color: "white"}}/> 
+            : <MenuIcon onClick={()=>toggleMenu()} style={{width: "50px", height: "50px", color: "white"}}/>}
+        </div>
+    </div>
+    {openMenu && <MenuLinks setopenMenu={setopenMenu}/> }
     </div>
   )
 }
---
+++ components/MenuLinks.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/MenuLinks.module.css'
+
+const MenuLinks = (props) => {
+  return (
+    <div className={styles.container}>
+        <div onClick={()=>props.setopenMenu(false)} className={styles.linkhover}>
+            <Link href="/">Home</Link>
+        </div>
+        <div onClick={()=>props.setopenMenu(false)} className={styles.linkhover}>
+            <Link href="/about">About</Link>
+        </div>
+        <div onClick={()=>props.setopenMenu(false)} className={styles.linkhover}>
+            <Link href="/projects">Projects</Link>
+        </div>
+        <div onClick={()=>props.setopenMenu(false)} className={styles.linkhover}>
+            <Link href="/resume">Resume</Link>
+        </div>
+    </div>
+  )
+}
+
+export default MenuLinks
