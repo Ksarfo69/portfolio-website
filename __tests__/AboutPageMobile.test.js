576a688366fae6ec6e1e1cecee17c6783cbe8d1c
Ksarfo69 <ksarfo69@gmail.com>
2022-07-30 07:52:15 -0700

    test: about page for mobile

--- __tests__/AboutPageMobile.test.js
+++ __tests__/AboutPageMobile.test.js
@@ -2,27 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/react'
 import '@testing-library/jest-dom'
-import About from '../pages/about'
+import About from '../pages/aboutPageForIndex'
 
 describe(About, ()=>{
     it("checks if the whole component has been rendered correctly", ()=>{
         render(<About/>)
-        const container = screen.getByTestId("aboutcontainer");
+        const container = screen.getByTestId("mobileaboutcontainer");
         const title = screen.getByTestId("title");
         const navbar = screen.getByTestId("navbar");
-        const image1 = screen.getByTestId("image1");
+        const image1 = screen.getByTestId("image");
         const info = screen.getByTestId("info");
         const image2 = screen.getByTestId("image2");
-        const footer = screen.getByTestId("footer");
-        
+                      
 
     
         expect(container).toBeInTheDocument();
         expect(title).toHaveTextContent("About");
         expect(navbar).toBeInTheDocument();
-        expect(image1 ).toBeInTheDocument();
+        expect(image1).toBeInTheDocument();
         expect(info).toBeInTheDocument();
         expect(image2).toBeInTheDocument();
-        expect(footer).toBeInTheDocument();
+        
     })
-})
+}) 
--- pages/aboutPageForIndex.js
+++ pages/aboutPageForIndex.js
@@ -2,18 +2,19 @@ import styles from '../styles/AboutIndex.module.css'
 import Navbar from '../components/Navbar'
 import Image from 'next/image'
 import Menu from '../components/Menu'
+import React from 'react'
 
 const About = () => {
   return (
-    <div className={styles.container}>
-      <Navbar/>
+    <div className={styles.container} data-testid="mobileaboutcontainer">
+      <div data-testid="navbar"><Navbar/></div>
       <div className={styles.wrapper}>
-        <div className={styles.left}>
+        <div className={styles.left} data-testid="image">
               <Image src="/Img/Aboutimg1.png" alt="" width={400} height={280} />
         </div>
         <div className={styles.center}>
-          <h1 className={styles.h1text}>About</h1>
-          <p className={styles.ptext}>
+          <h1 className={styles.h1text} data-testid="title">About</h1>
+          <p className={styles.ptext} data-testid="info">
             <p>
               I&apos;m a self taught fullstack developer with experience in designing and building management systems as well as deploying and managing web applications.
             </p>
@@ -25,7 +26,7 @@ const About = () => {
             </p>
           </p>
         </div>
-        <div className={styles.right}>
+        <div className={styles.right} data-testid="image2">
           <Image src="/Img/Aboutimg2.png" alt="" width={350} height={280} />
         </div>
       </div>
